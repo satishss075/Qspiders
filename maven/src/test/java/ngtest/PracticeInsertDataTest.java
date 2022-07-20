ddbc2b8b59979cbcdfe8bb15226c9a59e0b23bcc
Satish <Satish@DESKTOP-HTDTJ7L>
2022-07-20 16:27:16 +0530

    bbbbbbbbbbbb

--- maven/src/test/java/ngtest/PracticeInsertDataTest.java
+++ maven/src/test/java/ngtest/PracticeInsertDataTest.java
@@ -14,7 +14,8 @@ public class PracticeInsertDataTest {
 		String browser1 = System.getProperty("Browser");
 		String url2 = System.getProperty("Url");
 
-
+System.out.println("Browser" + " is " + browser1);
+System.out.println("url" + " is "+ url2);
 		
 		System.out.println("USERNAME"+ "  is equal to "  + " is " + username);
 		System.out.println("PASSWORD" + " is equal to " + " " + " is " + password );
