6a1bd23cc67d9de48e27bdcffc7fbef9591cdf73
Satish <Satish@DESKTOP-HTDTJ7L>
2022-07-20 16:39:37 +0530

    ccccccccc

--- maven/src/test/java/ngtest/PracticeInsertDataTest.java
+++ maven/src/test/java/ngtest/PracticeInsertDataTest.java
@@ -7,19 +7,20 @@ public class PracticeInsertDataTest {
 	@Test
 	public  void GetValueTest() 
 	{
-		String username = System.getProperty("USERNAME");
-		String password = System.getProperty("PASSWORD");
-		String browser = System.getProperty("BROWSER");
+		//String username = System.getProperty("USERNAME");
+		//String password = System.getProperty("PASSWORD");
+		//String browser = System.getProperty("BROWSER");
 		
 		String browser1 = System.getProperty("Browser");
 		String url2 = System.getProperty("Url");
 
-System.out.println("Browser" + " is " + browser1);
-System.out.println("url" + " is "+ url2);
+System.out.println( browser1);
+System.out.println( url2);
 		
-		System.out.println("USERNAME"+ "  is equal to "  + " is " + username);
-		System.out.println("PASSWORD" + " is equal to " + " " + " is " + password );
-		System.out.println("BROWSER" + " is equal to "  + " is " + browser);
+
+		//System.out.println("USERNAME"+ "  is equal to "  + " is " + username);
+		//System.out.println("PASSWORD" + " is equal to " + " " + " is " + password );
+		//System.out.println("BROWSER" + " is equal to "  + " is " + browser);
 
 		System.out.println("hello");
 		System.out.println("Byee");
